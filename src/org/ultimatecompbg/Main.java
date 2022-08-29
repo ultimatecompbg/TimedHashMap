0b9d633e1475f24de9df4cb0f80ac2e5192f7e18
ultimatecompbg <socialen_blog@outlook.com>
2022-08-29 12:19:55 +0100

    Initial commit

---
+++ src/org/ultimatecompbg/Main.java
@@ -0,0 +1,38 @@
+package org.ultimatecompbg;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        TimeoutCache newCacheMap = new TimeoutCache();
+        ObjectKiller objectKiller = new ObjectKiller(newCacheMap);
+        System.out.println("Object timeout(in milliseconds):");
+        Scanner timeOut = new Scanner(System.in);
+        int timeOutTime = timeOut.nextInt();
+        objectKiller.setTime(timeOutTime);
+        objectKiller.start();
+        System.out.println(objectKiller.getState());
+
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            String[] command = scanner.nextLine().split(" ");
+            if(Objects.equals(command[0], "end")){
+                objectKiller = null;
+                break;
+            }
+            if(Objects.equals(command[0], "put")){
+                newCacheMap.put(command[1]);
+            }
+            if(Objects.equals(command[0], "get")){
+                System.out.println(newCacheMap.get(command[1]));
+            }
+            if(Objects.equals(command[0], "remove")){
+                newCacheMap.remove(command[1]);
+            }
+            if(Objects.equals(command[0], "print")){
+                newCacheMap.print();
+            }
+        }
+    }
+}
---
+++ src/org/ultimatecompbg/TimeoutCache.java
@@ -0,0 +1,30 @@
+package org.ultimatecompbg;
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class TimeoutCache {
+    private HashMap<String, Timestamp> cacheMap = new HashMap<>();
+    public void checkAll(int timeOut){
+        Iterator<Map.Entry<String, Timestamp>> iterator = cacheMap.entrySet().iterator();
+        while(iterator.hasNext()) {
+            if(System.currentTimeMillis() - iterator.next().getValue().getTime() >= timeOut){
+                iterator.remove();
+            }
+        }
+    }
+    public void print() {
+
+    }
+    public void put(String key){
+        cacheMap.put(key, new Timestamp(System.currentTimeMillis()));
+    }
+    public Timestamp get(String key){
+        return cacheMap.get(key);
+    }
+    public void remove(String key){
+        cacheMap.remove(key);
+    }
+}

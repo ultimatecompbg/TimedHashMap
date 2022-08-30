8d4639754235579699b64db1ac2b506d360df191
ultimatecompbg <socialen_blog@outlook.com>
2022-08-30 11:29:44 +0100

    - TimeoutCache class is checking for expired objects instead of ObjectKiller - ObjectKiller class removed - TimeoutCache now runnable

--- src/org/ultimatecompbg/Main.java
+++ src/org/ultimatecompbg/Main.java
@@ -5,33 +5,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        TimeoutCache newCacheMap = new TimeoutCache();
-        ObjectKiller objectKiller = new ObjectKiller(newCacheMap);
         System.out.println("Object timeout(in milliseconds):");
         Scanner timeOut = new Scanner(System.in);
         int timeOutTime = timeOut.nextInt();
-        objectKiller.setTime(timeOutTime);
+        TimeoutCache newCacheMap = new TimeoutCache(timeOutTime);
+        Thread objectKiller = new Thread(newCacheMap);
         objectKiller.start();
         System.out.println(objectKiller.getState());
 
         while(true){
             Scanner scanner = new Scanner(System.in);
             String[] command = scanner.nextLine().split(" ");
+            // Sets the thread to null and breaks the loop
             if(Objects.equals(command[0], "end")){
                 objectKiller = null;
                 break;
             }
+            // Put new entry in the HashMap(command[1], <command[2], current time>)
             if(Objects.equals(command[0], "put")){
-                newCacheMap.put(command[1]);
+                newCacheMap.put(command[1], command[2]);
             }
+            // Get entry with key "command[1]"
             if(Objects.equals(command[0], "get")){
                 System.out.println(newCacheMap.get(command[1]));
             }
+            // Remove entry with key "command[1]"
             if(Objects.equals(command[0], "remove")){
                 newCacheMap.remove(command[1]);
-            }
-            if(Objects.equals(command[0], "print")){
-                newCacheMap.print();
             }
         }
     }
--- src/org/ultimatecompbg/TimeoutCache.java
+++ src/org/ultimatecompbg/TimeoutCache.java
@@ -3,33 +3,62 @@ package org.ultimatecompbg;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
-public class TimeoutCache {
-    private HashMap<String, Timestamp> cacheMap = new HashMap<>();
-    public void checkAll(int timeOut){
-        Iterator<Map.Entry<String, Timestamp>> iterator = cacheMap.entrySet().iterator();
-        while(iterator.hasNext()) {
-            if(System.currentTimeMillis() - iterator.next().getValue().getTime() >= timeOut){
-                iterator.remove();
+import static java.lang.Thread.sleep;
+
+public class TimeoutCache implements Runnable {
+    private int timeOut;
+    public TimeoutCache(int newTimeOut){
+        timeOut = newTimeOut;
+    }
+    private HashMap<String, HashMap<Object, Timestamp>> cacheMap = new HashMap<>();
+    public void run()
+    {
+        // Checks for expired objects in the HashMap every 10 milliseconds
+        while (true) {
+            try{
+                sleep(10);
+            }
+            catch(InterruptedException e){}
+            Iterator<Map.Entry<String, HashMap<Object, Timestamp>>> iterator = cacheMap.entrySet().iterator();
+            // Iterates through the HashMap
+            while(iterator.hasNext()) {
+                // If current time - next.timestamp > timeout, remove
+                if(System.currentTimeMillis() - iterator.next().getValue().entrySet().stream().findFirst().get().getValue().getTime() >= timeOut){
+                    iterator.remove();
+                }
             }
         }
-    }
-    public void print() {
 
     }
-    public void put(String key){
-        cacheMap.put(key, new Timestamp(System.currentTimeMillis()));
+    // Put object in the hashmap (key, <object, current time>)
+    public void put(String key, Object object){
+        HashMap<Object, Timestamp> newKeyValue = new HashMap<>();
+        newKeyValue.put(object, new Timestamp(System.currentTimeMillis()));
+        cacheMap.put(key, newKeyValue);
     }
-    public Timestamp get(String key){
+    // get entry with key "string key"
+    public Object get(String key){
         try{
-            return cacheMap.get(key);
-        }finally{
-            cacheMap.put(key, new Timestamp(System.currentTimeMillis()));
+            return cacheMap.get(key).entrySet().stream().findFirst().get().getKey();
+        }
+        catch(NullPointerException e){
+            return null;
+        }
+        finally{
+            if(cacheMap.get(key) != null){
+                HashMap<Object, Timestamp> newKeyValue = new HashMap<>();
+                newKeyValue.put(cacheMap.get(key).entrySet().stream().findFirst().get().getKey(), new Timestamp(System.currentTimeMillis()));
+                cacheMap.put(key, newKeyValue);
+            }
+
         }
 
 
     }
+    // remove entry with key "string key"
     public void remove(String key){
         cacheMap.remove(key);
     }
